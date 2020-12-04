71ecb3585208450d6877e0cb4da71721d2ced899
Bas <30810@ma-web.nl>
2020-12-04 14:35:45 +0100

    je kan ik een pauze nemen

--- gimme_a_break/break.js
+++ gimme_a_break/break.js
@@ -1,19 +1,24 @@
 i = document.getElementById("break_img")
 p = document.getElementById("clock")
 
-breakStart = new Date(2020, 11, 27, 15, 30, 0, 0)
-breakEnd = new Date(2020, 11, 27, 16, 30, 0, 0)
+breakStart = new Date(2020, 12, 4, 15, 45, 0, 0)
+breakEnd = new Date(2020, 12, 4, 21, 15, 0, 0)
 
 e = setInterval(updateTime,1000)
 
 function updateTime(){
     d = new Date()
      p.innerHTML = "Break Time from "+breakStart.toTimeString()+" until " +breakEnd.toTimeString()+"<br/> Time: "+d.getHours() +" : "+ d.getMinutes() +" : "+ d.getSeconds()
+    if(breakStart.getMinutes() == d.getMinutes()){
+        breakNow();
+    }
+    if (breakEnd.getMinutes() == d.getMinutes()){
+        breakNot();
+    }
 }
 function breakNow(){
     i.src = "break.jpg"
-   
 }
-function endBreak(){
+function breakNot(){
    i.src = "work.jpg"
 }
